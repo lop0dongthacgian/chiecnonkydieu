dbcbffe60e1fc09f3f94c2dcfb1e1d951a2edda9
lop0dongthacgian <lop0dongthacgian@gmail.com>
2025-06-13 17:12:51 +0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,28 @@
+// questions.js
+const allQuestions = [
+  {
+    question: "Chủ tịch Hồ Chí Minh đọc Tuyên ngôn Độc lập tại quảng trường nào?",
+    answer: ["B", "A", " ","Đ", "Ì", "N", "H"],
+    plainAnswer: ["B", "A"," ", "D", "I", "N", "H"]
+  },
+  {
+    question: "Thủ đô của Việt Nam là gì?",
+    answer: ["H", "À"," ", "N", "Ộ", "I"],
+    plainAnswer: ["H", "A", " ", "N", "O", "I"]
+  },
+  {
+    question: "Loài động vật nào được mệnh danh là 'chúa tể rừng xanh'?",
+    answer: ["S", "Ư", " ", "T", "Ử"],
+    plainAnswer: ["S", "U", " ", "T", "U"]
+  },
+  {
+    question: "Tên một loại trái cây màu đỏ, có hạt đen nhỏ bên ngoài?",
+    answer: ["D", "Â", "U", " ", "T", "Â", "Y"],
+    plainAnswer: ["D", "A", "U", " ", "T", "A", "Y"]
+  },
+  {
+    question: "Ngày Quốc Khánh Việt Nam là ngày bao nhiêu?",
+    answer: ["2", " ", "T", "H", "Á", "N", "G", " ", "9"],
+    plainAnswer: ["2", " ", "T", "H", "A", "N", "G", " ", "9"]
+  }
+];
